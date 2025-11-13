ba796f570021029b9c521996268dcbdc6de9f4b5
Abdulrahmanfawzy <abdulrahman2d77@gmail.com>
2025-11-13 20:20:57 +0200

    add persist through zustand to handle storage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,10 @@
 import { create } from "zustand";
 import { bookSlice } from "./slices/book.slice";
+import { persist } from "zustand/middleware";
 
-export const useAppStore = create((...a) => ({ ...bookSlice(...a) }));
+export const useAppStore = create(
+  persist((...a) => ({ ...bookSlice(...a) }), {
+    name: "books",
+    getStorage: () => localStorage,
+  })
+);
--- src/store/slices/book.slice.ts
+++ src/store/slices/book.slice.ts
@@ -1,34 +1,34 @@
-import type { IBook } from "../../types/book.type";
+    import type { IBook } from "../../types/book.type";
 
-export interface IBookSlice {
-  books: IBook[];
-  book: IBook | null;
-  bookUpdate: IBook | null;
-  addBook: (book: IBook) => void;
-  readBook: (book: IBook) => void;
-  deleteBook: (id: number) => void;
-  editBook: (book: IBook | null) => void;
-  updateBookInBooksArr: (updatedBook: IBook) => void;
-}
+    export interface IBookSlice {
+    books: IBook[];
+    book: IBook | null;
+    bookUpdate: IBook | null;
+    addBook: (book: IBook) => void;
+    readBook: (book: IBook) => void;
+    deleteBook: (id: number) => void;
+    editBook: (book: IBook | null) => void;
+    updateBookInBooksArr: (updatedBook: IBook) => void;
+    }
 
-export const bookSlice = (set, get) => ({
-  books: [],
-  book: null,
-  bookUpdate: null,
-  addBook: (book: IBook) => set((state) => ({ books: [...state.books, book] })),
-  readBook: (book) => {
-    const bookToRead = get().books.find((el: IBook) => el.id === book.id);
-    set({ book: bookToRead });
-  },
-  deleteBook: (id: number) =>
-    set({ books: get().books.filter((el) => el.id !== id) }),
+    export const bookSlice = (set, get) => ({
+    books: [],
+    book: null,
+    bookUpdate: null,
+    addBook: (book: IBook) => set((state) => ({ books: [...state.books, book] })),
+    readBook: (book) => {
+        const bookToRead = get().books.find((el: IBook) => el.id === book.id);
+        set({ book: bookToRead });
+    },
+    deleteBook: (id: number) =>
+        set({ books: get().books.filter((el) => el.id !== id) }),
 
-  editBook: (book) => set({ bookUpdate: book }),
+    editBook: (book) => set({ bookUpdate: book }),
 
-  updateBookInBooksArr: (updatedBook: IBook) =>
-    set((state) => ({
-      books: state.books.map((book) =>
-        book.id === updatedBook.id ? updatedBook : book
-      ),
-    })),
-});
+    updateBookInBooksArr: (updatedBook: IBook) =>
+        set((state) => ({
+        books: state.books.map((book) =>
+            book.id === updatedBook.id ? updatedBook : book
+        ),
+        })),
+    });
